829ed635eb4754cbb741bb57a839daa90f1a42a2
prahal <prahal@users.noreply.github.com>
2021-10-26 21:23:01 +1100

    fetching all objectIDs from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+import { BounceLoader } from 'react-spinners';
 
 import './App.css';
 
 const App = () => {
+  const intervalRef = React.useRef(null);
+  const [loading, setLoading] = React.useState(false);
+  const [objectIDs, setObjectIDs] = React.useState([]);
+
+  const baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects`;
+
+  const fetchObjectIDs = async () => {
+    setLoading(true);
+    const {objectIDs} = (await axios.get(baseURL)).data.objectIDs; 
+    setObjectIDs(objectIDs);
+    setLoading(false);
+  }; 
+
   return (
-    <div className="App">
-    </div>
+    loading ? 
+      <div className="spinner"><BounceLoader size="100px" /></div> :
+      <div className="app">
+      </div>
   );
 }
 
