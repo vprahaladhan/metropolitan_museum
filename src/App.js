1fbe8ab10ad22724a0251c9eedd104cb65284a12
prahal <prahal@users.noreply.github.com>
2021-10-27 05:46:40 +1100

    b4 adding an object details modals using bootstrap

--- src/App.js
+++ src/App.js
@@ -6,17 +6,54 @@ import './App.css';
 
 const App = () => {
   const intervalRef = React.useRef(null);
+  const [index, setIndex] = React.useState(0);
   const [loading, setLoading] = React.useState(false);
+  const [isPaused, setIsPaused] = React.useState(false); 
   const [objectIDs, setObjectIDs] = React.useState([]);
+  const [currentObject, setCurrentObject] = React.useState();
 
   const baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects`;
 
+  React.useEffect(() => {
+    fetchObjectIDs();
+  }, []);
+
+  React.useEffect(() => {
+    !!objectIDs.length && fetchObject() && start();
+  }, [objectIDs]);
+
+  React.useEffect(() => {
+    !!objectIDs.length && fetchObject();
+  }, [index]);
+
   const fetchObjectIDs = async () => {
     setLoading(true);
-    const {objectIDs} = (await axios.get(baseURL)).data.objectIDs; 
+    const {objectIDs} = (await axios.get(baseURL)).data; 
     setObjectIDs(objectIDs);
     setLoading(false);
-  }; 
+  };
+
+  const fetchObject = async () => {
+    setCurrentObject((await axios.get(`${baseURL}/${objectIDs[index]}`)).data);
+  }
+
+  const start = () => {
+    intervalRef.current = setInterval(() => {
+      setIndex(index => index + 1);
+    }, 10000);
+  };
+
+  const pause = () => {
+    clearInterval(intervalRef.current);
+    setIsPaused(true);
+  };
+
+  const resume = () => {
+    setIsPaused(false);
+    intervalRef.current = setInterval(() => {
+      setIndex(index => index + 1);
+    }, 10000);
+  };
 
   return (
     loading ? 
